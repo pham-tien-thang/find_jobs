672b2e357e4c2723a651ddf6951d5eaf340ba2b0
DESKTOP-MC\Admin <gitkudo0504@gmail.com>
2021-03-31 01:06:23 +0700

    no message

--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -9,19 +9,12 @@ import 'package:find_jobs/layout_home/Header_home.dart';
 import 'package:find_jobs/layout_home/Newest_candicate.dart';
 import 'package:find_jobs/layout_home/Newest_jobs.dart';
 import 'package:find_jobs/layout_home/Slider.dart';
-<<<<<<< HEAD
-import 'package:find_jobs/screen/Search_keywword.dart';
-=======
-import 'package:find_jobs/main.dart';
-import 'package:find_jobs/screen/LoginScreen.dart';
->>>>>>> parent of 0c03594 (thang commit profile)
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-
 import 'Detail_user.dart';
 import 'SignUp.dart';
 
